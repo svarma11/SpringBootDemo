71abd3fc2eb63172713dab46ead1b512130ef49a
svarma11 <sujitvarma2020@gmail.com>
2025-06-14 18:57:12 +0530

    added the user contractor id

--- src/main/java/com/example/demo/mapper/TimesheetMapper.java
+++ src/main/java/com/example/demo/mapper/TimesheetMapper.java
@@ -3,6 +3,7 @@ package com.example.demo.mapper;
 import com.example.demo.dto.TimesheetDto;
 import com.example.demo.model.Timesheet;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
@@ -10,6 +11,7 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public interface TimesheetMapper {
     TimesheetMapper INSTANCE= Mappers.getMapper(TimesheetMapper.class);
+    @Mapping(target = "contractorId", source = "timesheet.contractor.id")
     TimesheetDto toDto(Timesheet timesheet);
     Timesheet toEntity(TimesheetDto timesheetDto);
     List<TimesheetDto> toDtoList(List<Timesheet> timesheets);
